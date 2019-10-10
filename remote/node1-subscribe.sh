5e75bf53d31d4c36a31e1722a42a26704f758045
denis lussier <denisl@oscg-partners.com>
2019-10-10 02:26:29 -0700

    fix typo

--- remote/node1-subscribe.sh
+++ remote/node1-subscribe.sh
@@ -3,7 +3,7 @@ source env.sh
 
 PSQL="$psql -h $NODE1 demo"
 
-$PSQL -c "SELECT pgpsock.create_subscription( \
+$PSQL -c "SELECT pgspock.create_subscription( \
     subscription_name := 'subscription2', \
     provider_dsn := 'host=$NODE2_IP port=5432 user=replication password=password dbname=demo', \
     forward_origins := '{}', \
@@ -11,10 +11,10 @@ $PSQL -c "SELECT pgpsock.create_subscription( \
 );"
 
 
-$PSQL -c "SELECT pgpsock.alter_subscription_add_replication_set( \
+$PSQL -c "SELECT pgspock.alter_subscription_add_replication_set( \
     subscription_name := 'subscription2', \
     replication_set := 'bmsql_set' \
 );"
 
 
-$PSQL -c "SELECT pgpsock.wait_for_subscription_sync_complete('subscription2');"
+$PSQL -c "SELECT pgspock.wait_for_subscription_sync_complete('subscription2');"
