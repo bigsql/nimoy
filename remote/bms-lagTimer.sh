e2d3b56c5857720aef98dcb23a93356b9125477b
denis lussier <denisl@oscg-partners.com>
2019-10-14 16:57:11 -0700

    2nd pass

--- remote/bms-lagTimer.sh
+++ remote/bms-lagTimer.sh
@@ -1,16 +1,25 @@
 
 source env.sh 
 
+psql=/home/ec2-user/bigsql/pg11/bin/psql
+
 SELECT="select pg_size_pretty(pg_wal_lsn_diff(pg_current_wal_lsn(), replay_lsn)) from pg_stat_replication"
-sleep 1
+sleep 2
 timer=1
+echo ""
 
 while true; do
-  total_lag=`echo "$SELECT" | psql -qAt -U postgres -h $NODE1 -d demo
-  if [ "$total_lag" == "0" ]; then
-    exit 0
+  total_lag=`echo "$SELECT" | $psql -qAt -U postgres -h $ip_n1 -d demo`
+  if [ "$total_lag" == "0 bytes" ]; then
+    if [ "$timer" == "1" ] || [ "$timer" == "2" ]; then
+      echo "Total Lag less than 2(s)"
+      exit 0
+    else
+      echo "Total Lag equals approx $timer(s)"
+      exit 0
+    fi
   else
-    echo "total_lag = $total_lag after $timer (s)"
+    echo "Total Lag equals $total_lag after $timer(s)"
     sleep 1
     timer=$(( $timer + 1 ))
   fi
